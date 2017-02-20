5d6a00f4b8df1d0a2111bd05a091943b431546b2
Daniel Haro <danielharo@MacBook-Pro-de-Daniel.local>
2017-02-19 21:37:06 -0600

    share the git repository

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
-/**
- * Created by danielharo on 12/02/17.
- */
+$(function() {
+    $("#navbar li").click(function(){
+        $("#navbar li").removeClass( "active" );
+        $(this).addClass("active");
+    });
+    $(".navbar-header").click(function(){
+        $("#navbar li").removeClass( "active" );
+        $(this).addClass("active");
+    });
+});
