691ed5dc4b6af60e2ec7cfafba680187a0f96ffd
Daniel Haro <danielharo@MacBook-Pro-de-Daniel.local>
2017-03-05 11:31:53 -0600

    Solving issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,20 @@ $(function() {
         $("#navbar li").removeClass( "active" );
         $(this).addClass("active");
     });
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if(scroll >= 1025){
+            $("#navbar li").removeClass( "active" );
+            $("#nav-hobbies").addClass("active");
+        }
+        else if(scroll >= 850){
+            $("#navbar li").removeClass( "active" );
+            $("#nav-skill").addClass("active");
+        }
+        else{
+            $("#navbar li").removeClass( "active" );
+            $("#nav-about").addClass("active");
+        }
+    });
 });
